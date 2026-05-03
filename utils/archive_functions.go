660fa3fa464d09972d8c5f7978ad452313fa55ea
MertJSX <mertJSX@proton.me>
2026-05-03 17:16:50 +0300

    chore: remove unused comments

--- utils/archive_functions.go
+++ utils/archive_functions.go
@@ -16,12 +16,6 @@ func Unzip(src, dest string, cb func(int64, bool, string)) error {
 	}
 	defer r.Close()
 
-	// err = os.MkdirAll(dest, 0777)
-
-	// if err != nil {
-	// 	return fmt.Errorf("cannot create folder: %v", err)
-	// }
-
 	var (
 		totalSize  int64 = 0
 		currentUID int   = os.Getuid()
